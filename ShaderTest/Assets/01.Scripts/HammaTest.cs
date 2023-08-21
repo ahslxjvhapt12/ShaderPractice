bf987b7847e865febf57cd9b9ba55595adea9ff1
ahslxjvhapt12 <likjhee1@gmail.com>
2023-08-21 15:39:59 +0900

    셰이더 코드로 머시기

--- ShaderTest/Assets/01.Scripts/HammaTest.cs
+++ ShaderTest/Assets/01.Scripts/HammaTest.cs
@@ -1,9 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class HammaTest : MonoBehaviour
 {
     SpriteRenderer _renderer;
+    private readonly int offsetHash = Shader.PropertyToID("_Offset");
 
+
+    private void Awake()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(1);
+            StartCoroutine(ShowCoroutine());
+        }
+    }
+
+    IEnumerator ShowCoroutine()
+    {
+        float t = 2f;
+        float ct = 0f;
+        float percent = 0;
+        float value;
+        while (percent < 1)
+        {
+            ct += Time.deltaTime;
+            percent = ct / t;
+            value = Mathf.Lerp(-1, 1, percent);
+            Debug.Log(value);
+            //Debug.Log(percent);
+            _renderer.material.SetFloat(offsetHash, value);
+            yield return null;
+        }
+    }
 }
