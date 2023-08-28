93f39dd7c9f9400704f2d6fb607d8604b95f293f
ahslxjvhapt12 <likjhee1@gmail.com>
2023-08-28 13:55:02 +0900

    Input

--- InputSystem/Assets/01.Scripts/PlayerController.cs
+++ InputSystem/Assets/01.Scripts/PlayerController.cs
@@ -1,20 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     Rigidbody _rigid;
+    private PlayerInput playerInput;
     Vector3 power = Vector3.up * 5f;
 
     private void Awake()
     {
         _rigid = GetComponent<Rigidbody>();
+        playerInput = GetComponent<PlayerInput>();
+
+        playerInput.onActionTriggered += OnInputActionTriggered;
     }
 
-    public void Jump()
+    private void OnInputActionTriggered(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void Jump(InputAction.CallbackContext context)
     {
         Debug.Log(".га");
-        _rigid.AddForce(power, ForceMode.Impulse);
+        if (context.started)
+        {
+            _rigid.AddForce(power, ForceMode.Impulse);
+        }
     }
 }
