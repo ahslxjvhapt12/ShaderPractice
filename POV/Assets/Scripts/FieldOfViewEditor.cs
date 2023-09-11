1d93827db5521639aa07f8e4d62572fd98d90e6c
ahslxjvhapt12 <likjhee1@gmail.com>
2023-09-11 16:24:02 +0900

    DrawGizmo

--- POV/Assets/Scripts/FieldOfViewEditor.cs
+++ POV/Assets/Scripts/FieldOfViewEditor.cs
@@ -11,5 +11,17 @@ public class FieldOfViewEditor : Editor
         FieldOfView fow = (FieldOfView)target;
         Handles.color = Color.white;
         Handles.DrawWireArc(fow.transform.position, Vector3.up, Vector3.forward, 360, fow.viewRadius);
+
+        Vector3 viewAngleA = fow.DirFromAngle(-fow.viewAngle / 2, true);
+        Vector3 viewAngleB = fow.DirFromAngle(+fow.viewAngle / 2, true);
+
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleA * fow.viewRadius);
+        Handles.DrawLine(fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
+
+        Handles.color = Color.red;
+        foreach (var item in fow.visibleTargets)
+        {
+            Handles.DrawLine(fow.transform.position, item.position);
+        }
     }
 }
